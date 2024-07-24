1fe99dffccac9a92afc678ce5ea67d9057ce1c1d
Lauren Viado <lauren.viado1@gmail.com>
2024-07-24 11:30:35 -0400

    updateJS

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('email').addEventListener('click', function() {
+    window.location.href = 'mailto:viadolm@vcu.edu';
+})
+
+document.getElementById('linkedin').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/lauren-viado/', '_blank');
+})
+
+document.getElementById('github').addEventListener('click', function() {
+    window.open('https://github.com/laurenviado', '_blank');
+});
